966e670c64af1f2c0ce8f43c5b28ae28787f73be
swedkg <dimitrios.giannopoulos@gmail.com>
2018-10-11 08:15:07 +0200

    users imported, modiified users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_153703) do
+ActiveRecord::Schema.define(version: 2018_10_09_184551) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2018_08_28_153703) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "activity_types", force: :cascade do |t|
+    t.string "label"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "authentication_tokens", force: :cascade do |t|
@@ -48,6 +54,8 @@ ActiveRecord::Schema.define(version: 2018_08_28_153703) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create([
+#     {
+#       "id": 11,
+#       "first_name": "Sebastian",
+#       "last_name": "Eschweiler",
+#       "email": "sebastian@codingthesmartway.com",
+#       "password": "jklgjkfdsljkl;jdfg"
+#     },
+#     {
+#       "id": 12,
+#       "first_name": "Steve",
+#       "last_name": "Palmer",
+#       "email": "steve@codingthesmartway.com",
+#       "password": "jklgjkfdsljkl;jdfg"
+#     },
+#     {
+#       "id": 13,
+#       "first_name": "Ann",
+#       "last_name": "Smith",
+#       "email": "ann@codingthesmartway.com",
+#       "password": "jklgjkfdsljkl;jdfg"
+#     },
+#     {
+#       "first_name": "Dimitris",
+#       "last_name": "Giannopoulos",
+#       "email": "dimitrios.giannopoulos@gmail.com",
+#       "password": "jklgjkfdsljkl;jdfg",
+#       "id": 14
+#     }
+#   ])
+
+json = ActiveSupport::JSON.decode(File.read('db.json'))
+
+json["activities_types"].each do |a|
+  Country.create!(a['country'], without_protection: true)
+end
