87b5b7a0dabfcc8c1868f01ed623be4393cfe08d
swedkg <dimitrios.giannopoulos@gmail.com>
2018-12-21 21:16:27 +0100

    'mod'

--- db/migrate/20181016172406_change_date_and_duration_type_to_number.rb
+++ db/migrate/20181016172406_change_date_and_duration_type_to_number.rb
@@ -1,6 +1,6 @@
 class ChangeDateAndDurationTypeToNumber < ActiveRecord::Migration[5.2]
   def change
-    change_column :activities, :date, "integer using date:integer"
+    change_column :activities, :date, :integer, using: "date::integer"
     change_column :activities, :duration, :integer
   end
 end
